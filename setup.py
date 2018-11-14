b1f1f216b233ac3678e29a04faff3c4345718222
Curtis Rueden <ctrueden@wisc.edu>
2018-11-14 13:46:59 -0600

    Update package metadata
    * Use README.md for long description
    * Add myself as co-author
    * Add license

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from distutils.core import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md')) as f:
+    javarun_long_description = f.read()
 
 setup(
     name='javarun',
     version='0.1.0-dev',
-    author='Philipp Hanslovsky',
+    author='Philipp Hanslovsky, Curtis Rueden',
     author_email='hanslovskyp@janelia.hhmi.org',
     description='Launch Java code from Python and the CLI, installation-free.',
+    long_description=javarun_long_description,
+    long_description_content_type='text/markdown',
+    license='Public domain',
     url='https://github.com/scijava/jrun',
     packages=['javarun'],
     entry_points={
