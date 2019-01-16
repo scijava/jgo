39be8c6e4c451926e115061658984a90c40bff2d
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-01-16 11:11:16 -0500

    Test version in .travis/build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,3 +3,4 @@ pip install .
 python -c 'import sys; sys.path.remove(""); import jgo'
 which jgo
 test "$(jgo org.scijava:parsington 1+3)" -eq 4
+test "$(python -c 'from jgo import __version__; print(__version__)')" = "$(jgo --version)"
