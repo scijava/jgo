1e8931a57c8539654760958487ba1d02830d963f
Curtis Rueden <ctrueden@wisc.edu>
2021-05-04 13:27:03 -0500

    Release version 1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='jgo',
-    version='1.0.1.dev0',
+    version='1.0.1',
     author='Philipp Hanslovsky, Curtis Rueden',
     author_email='ctrueden@wisc.edu',
     description='Launch Java code from Python and the CLI, installation-free.',
