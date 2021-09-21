ecddc5f0ebfb709b654e075eeb06dddcf60a5828
hinerm <hinerm@gmail.com>
2021-09-21 12:14:27 -0500

    release.sh: add documentation of release process steps

--- release.sh
+++ release.sh
@@ -9,26 +9,34 @@ next_version() {
   echo "$prefix.$((suffix+1)).dev0"
 }
 
+# Check for .pypirc which is required for uploading via twine
 if [ ! -f ~/.pypirc ]; then
   die "Please set set up a .pypirc file with username and password in your user home directory before proceeding. See https://packaging.python.org/specifications/pypirc/"
 fi
 
+# Check for required commands
 for cmd in echo git grep mv python rm sed test twine; do
   which "$cmd" >/dev/null || die "Missing required tool: $cmd"
 done
 
+# Run the unit tests
 python setup.py test || die "Some tests failed!"
 
+# Update the version string to non-dev version
 sed 's/\.dev[0-9]\+//' setup.py > setup.py.new &&
 mv -f setup.py.new setup.py &&
 version=$(grep version= setup.py | sed "s/.*version=\"\([^\"]*\)\".*/\1/") &&
 test "$version" && echo "Releasing version: $version" ||
   die "Cannot glean version string!"
 
+# Create release commit
 git commit -m "Release version $version" setup.py &&
+# Clear old builds and re-build distribution
 rm -rf dist &&
 python setup.py sdist bdist_wheel &&
+# Create the release tag
 git tag "$version" &&
+# Bump to the next dev version
 sed "s/version=.*/version=\"$(next_version "$version")\",/" setup.py > setup.py.new &&
 mv -f setup.py.new setup.py &&
 git commit -m "Bump to next development cycle" setup.py &&
