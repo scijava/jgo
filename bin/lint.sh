3222bf0b6e1034538a86bdc3b7f1deb63a75d26b
Curtis Rueden <ctrueden@wisc.edu>
2025-07-20 12:00:39 -0500

    Make lint script return non-zero upon any change
    So that the CI fails when reformatting needed to occur.

--- bin/lint.sh
+++ bin/lint.sh
@@ -4,10 +4,19 @@ dir=$(dirname "$0")
 cd "$dir/.."
 
 exitCode=0
+
+# Check for errors and capture non-zero exit codes.
 uv run validate-pyproject pyproject.toml
 code=$?; test $code -eq 0 || exitCode=$code
+uv run ruff check >/dev/null 2>&1
+code=$?; test $code -eq 0 || exitCode=$code
+uv run ruff format --check >/dev/null 2>&1
+code=$?; test $code -eq 0 || exitCode=$code
+
+# Do actual code reformatting.
 uv run ruff check --fix
 code=$?; test $code -eq 0 || exitCode=$code
 uv run ruff format
 code=$?; test $code -eq 0 || exitCode=$code
+
 exit $exitCode
