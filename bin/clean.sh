4a35e4a66cdd75d1ffbb40652e2890096bc38674
Curtis Rueden <ctrueden@wisc.edu>
2026-01-01 16:17:58 -0600

    Delete mypy cache when cleaning

--- bin/clean.sh
+++ bin/clean.sh
@@ -6,4 +6,4 @@ cd "$dir/.."
 find . -name __pycache__ -type d | while read d
   do rm -rfv "$d"
 done
-rm -rfv .pytest_cache build dist src/*.egg-info
+rm -rfv .mypy_cache .pytest_cache build dist src/*.egg-info
