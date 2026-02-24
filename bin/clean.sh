eccef9509bd0d736a0fcff698c1990434fef7a45
Curtis Rueden <ctrueden@wisc.edu>
2026-02-24 09:17:26 -0600

    Clean docs build also when cleaning

--- bin/clean.sh
+++ bin/clean.sh
@@ -6,4 +6,12 @@ cd "$dir/.."
 find . -name __pycache__ -type d | while read d
   do rm -rfv "$d"
 done
-rm -rfv .cache .mypy_cache .pytest_cache .ruff_cache build dist src/*.egg-info
+rm -rfv \
+  .cache \
+  .mypy_cache \
+  .pytest_cache \
+  .ruff_cache \
+  build \
+  dist \
+  docs/_build \
+  src/*.egg-info
