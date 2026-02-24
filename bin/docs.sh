3b481add09ffaf9e43ff14b85af8f0bec2a79f6a
Curtis Rueden <ctrueden@wisc.edu>
2026-02-24 09:17:35 -0600

    Reuse docs/requirements.txt for local docs builds

--- bin/docs.sh
+++ bin/docs.sh
@@ -4,9 +4,5 @@ dir=$(dirname "$0")
 cd "$dir/.."
 
 uv run \
-  --with sphinx \
-  --with sphinx-rtd-theme \
-  --with "myst-parser>=3.0" \
-  --with sphinx-copybutton \
-  --with "sphinx-design>=0.6" \
+  --with-requirements docs/requirements.txt \
   sphinx-build -b html docs docs/_build/html
